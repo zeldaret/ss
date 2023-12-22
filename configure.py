1c36d30a720efbd3b2c32a59295bf8be4a64b67a
elijah-thomas774 <elijahthomas774@gmail.com>
2023-12-22 14:54:03 -0500

    updated some project tools from the template

--- configure.py
+++ configure.py
@@ -119,7 +119,7 @@ if not is_windows():
     config.wrapper = args.wrapper
 
 # Tool versions
-config.compilers_tag = "1"
+config.compilers_tag = "20231018"
 config.dtk_tag = "v0.6.5"
 config.sjiswrap_tag = "v1.1.1"
 config.wibo_tag = "0.6.9"
