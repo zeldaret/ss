d3e39ebc28e85751eaf03c2eca887194dabe6460
robojumper <robojumper@gmail.com>
2024-05-03 10:01:24 +0200

    Cleanup

--- src/toBeSorted/counters/rupee_counter.cpp
+++ src/toBeSorted/counters/rupee_counter.cpp
@@ -12,22 +12,19 @@ struct WalletStruct {
     u16 capacity;
 };
 
-/* 804E91B0 */
-const struct WalletStruct wallet_definitions[4] = {
-        {0x6c, 500},
-        {0x6d, 1000},
-        {0x6e, 5000},
-        {0x6f, 9000},
-};
-
 // TODO set up item flag manager
 extern "C" void *lbl_80575400;
 extern "C" u16 fn_800BF5E0(void *data, u16 flag);
 
 /* 8016DE10 */ u16 getBaseCapacity() {
     int i = 0;
-    WalletStruct tmp[4] = {wallet_definitions[0], wallet_definitions[1], wallet_definitions[2], wallet_definitions[3]};
-    const WalletStruct *wallet = &tmp[3];
+    /* 804E91B0 */ WalletStruct wallet_definitions[4] = {
+            {0x6c, 500},
+            {0x6d, 1000},
+            {0x6e, 5000},
+            {0x6f, 9000},
+    };
+    const WalletStruct *wallet = &wallet_definitions[3];
     for (; i < 4; i++, wallet--) {
         if (fn_800BF5E0(lbl_80575400, wallet->flag)) {
             return wallet->capacity;
