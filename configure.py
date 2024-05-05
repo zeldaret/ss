e1f491d2d13863434f785330b9830a63d338c3ce
elijah-thomas774 <elijahthomas774@gmail.com>
2024-05-04 22:02:12 -0400

    fix duplicated split oops

--- configure.py
+++ configure.py
@@ -333,7 +333,6 @@ config.libs = [
             Object(NonMatching, "nw4r/ut/ut_ResFont.cpp"),
             Object(NonMatching, "nw4r/ut/ut_CharWriter.cpp"),
             Object(NonMatching, "nw4r/ut/ut_TextWriterBase.cpp"),
-            Object(Matching, "nw4r/ut/ut_binaryFileFormat.cpp"),
         ],
     ),
     # EGG
