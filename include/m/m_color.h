e296019657b4edd227f85ff50c37061df4e35741
robojumper <robojumper@gmail.com>
2024-06-26 20:08:59 +0200

    mColor can extend ut::Color

--- include/m/m_color.h
+++ include/m/m_color.h
@@ -1,35 +1,9 @@
 #ifndef M_COLOR_H
 #define M_COLOR_H
 
+#include <nw4r/ut/ut_Color.h>
 #include <rvl/GX.h>
 
-struct mColor {
-    mColor() {
-        rgba = 0xFFFFFFFF;
-    }
-    mColor(u8 red, u8 green, u8 blue, u8 alpha) {
-        r = red;
-        g = green;
-        b = blue;
-        a = alpha;
-    }
-    mColor(u32 color) {
-        rgba = color;
-    }
+struct mColor : public nw4r::ut::Color {};
 
-    // The alternative is that GXColor is a union and this extends GXColor?
-    operator GXColor &() {
-        return reinterpret_cast<GXColor &>(*this);
-    }
-
-    union {
-        struct {
-            u8 r;
-            u8 g;
-            u8 b;
-            u8 a;
-        };
-        u32 rgba;
-    };
-};
 #endif
