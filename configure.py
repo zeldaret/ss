e6dd20ada146c5474409b3b89b2f7b0f8f399bf2
robojumper <robojumper@gmail.com>
2024-10-20 15:55:38 +0200

    dAcOutaDemoPedest_c, dAcOutajimaLv2_c OK (#75)
    * dAcOutaDemoPedest_c OK
    
    * dAcOutajimaLv2_c OK

--- configure.py
+++ configure.py
@@ -1709,7 +1709,7 @@ config.libs = [
     Rel(
         NonMatching, "d_a_obj_utajima_island", "REL/d/a/obj/d_a_obj_utajima_island.cpp"
     ),
-    Rel(NonMatching, "d_a_obj_utajima_lv2", "REL/d/a/obj/d_a_obj_utajima_lv2.cpp"),
+    Rel(Matching, "d_a_obj_utajima_lv2", "REL/d/a/obj/d_a_obj_utajima_lv2.cpp"),
     Rel(
         NonMatching,
         "d_a_obj_utajima_main_mecha",
@@ -1726,7 +1726,7 @@ config.libs = [
         "REL/d/a/obj/d_a_obj_utajima_stopper.cpp",
     ),
     Rel(
-        NonMatching,
+        Matching,
         "d_a_obj_uta_demo_pedest",
         "REL/d/a/obj/d_a_obj_uta_demo_pedest.cpp",
     ),
--- src/REL/d/a/obj/d_a_obj_uta_demo_pedest.cpp
+++ src/REL/d/a/obj/d_a_obj_uta_demo_pedest.cpp
@@ -1,3 +1,59 @@
 #include "d/a/obj/d_a_obj_uta_demo_pedest.h"
 
+#include "d/a/obj/d_a_obj_base.h"
+#include "f/f_base.h"
+#include "m/m_vec.h"
+#include "nw4r/g3d/g3d_resmdl.h"
+#include "toBeSorted/arc_managers/current_stage_arc_manager.h"
+#include "toBeSorted/room_manager.h"
+
+static const char *const sMdlNames[] = {
+    "StageF010rPedest",
+    "StageWindF001r",
+};
+
 SPECIAL_ACTOR_PROFILE(OBJ_UTA_DEMO_PEDEST, dAcOutaDemoPedest_c, fProfile::OBJ_UTA_DEMO_PEDEST, 0x212, 0, 6);
+
+bool dAcOutaDemoPedest_c::createHeap() {
+    mModelType = getModelType();
+    if (mModelType == 0xF) {
+        mModelType = 0;
+    }
+    if (mModelType > 1) {
+        mModelType = 0;
+    }
+
+    mRes = CurrentStageArcManager::sInstance->getData("g3d/stage.brres");
+    RoomManager::bindStageResToFile(&mRes);
+    RoomManager::bindSkyCmnToResFile(&mRes);
+    nw4r::g3d::ResMdl mdl = mRes.GetResMdl(sMdlNames[mModelType]);
+    TRY_CREATE(mMdl.create(mdl, &heap_allocator, 0x120));
+
+    return true;
+}
+
+int dAcOutaDemoPedest_c::create() {
+    CREATE_ALLOCATOR(dAcOutaDemoPedest_c);
+
+    updateMatrix();
+    mMdl.setLocalMtx(mWorldMtx);
+    mMdl.setPriorityDraw(0x1C, 9);
+    mVec3_c l, u;
+    mMdl.getBounds(&l, &u);
+    boundingBox.Set(l, u);
+
+    return SUCCEEDED;
+}
+
+int dAcOutaDemoPedest_c::doDelete() {
+    return SUCCEEDED;
+}
+
+int dAcOutaDemoPedest_c::actorExecute() {
+    return SUCCEEDED;
+}
+
+int dAcOutaDemoPedest_c::draw() {
+    drawModelType1(&mMdl);
+    return SUCCEEDED;
+}
