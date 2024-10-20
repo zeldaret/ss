9bbe23fff0a09b749544540123f0e0531f8d93b4
robojumper <robojumper@gmail.com>
2024-10-20 20:51:14 +0200

    d_a_obj_fruit_guts_leaf OK

--- include/d/a/obj/d_a_obj_fruit_guts_leaf.h
+++ include/d/a/obj/d_a_obj_fruit_guts_leaf.h
@@ -2,13 +2,26 @@
 #define D_A_OBJ_FRUIT_GUTS_LEAF_H
 
 #include "d/a/obj/d_a_obj_base.h"
+#include "m/m3d/m_smdl.h"
 
 class dAcOFruitGutsLeaf_c : public dAcObjBase_c {
 public:
     dAcOFruitGutsLeaf_c() {}
     virtual ~dAcOFruitGutsLeaf_c() {}
 
+    virtual bool createHeap() override;
+    virtual int actorCreate() override;
+    virtual int actorPostCreate() override;
+    virtual int actorExecute() override;
+    virtual int draw() override;
+
 private:
+
+    s32 getType();
+    bool hideInPast();
+    s32 getPastBehavior();
+
+    /* 0x330 */ m3d::smdl_c mMdl;
 };
 
 #endif
