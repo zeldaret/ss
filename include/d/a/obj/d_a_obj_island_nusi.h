9d63fb8926e444805809aae0f8913f09e14a5059
robojumper <robojumper@gmail.com>
2024-10-24 23:40:08 +0200

    d_a_obj_island_nusi OK

--- include/d/a/obj/d_a_obj_island_nusi.h
+++ include/d/a/obj/d_a_obj_island_nusi.h
@@ -2,6 +2,9 @@
 #define D_A_OBJ_ISLAND_NUSI_H
 
 #include "d/a/obj/d_a_obj_base.h"
+#include "d/col/bg/d_bg_w.h"
+#include "m/m3d/m_smdl.h"
+#include "nw4r/g3d/g3d_resfile.h"
 #include "s/s_State.hpp"
 #include "s/s_StateMgr.hpp"
 
@@ -10,11 +13,24 @@ public:
     dAcOislandNusi_c() : mStateMgr(*this, sStateID::null) {}
     virtual ~dAcOislandNusi_c() {}
 
+    virtual bool createHeap() override;
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int actorExecute() override;
+    virtual int draw() override;
+
     STATE_FUNC_DECLARE(dAcOislandNusi_c, Wait);
     STATE_FUNC_DECLARE(dAcOislandNusi_c, NusiFight);
 
 private:
-    /* 0x??? */ STATE_MGR_DECLARE(dAcOislandNusi_c);
+    static const f32 someFloat;
+
+    bool isInLeviasFight() const;
+
+    /* 0x330 */ m3d::smdl_c mMdls[2];
+    /* 0x360 */ nw4r::g3d::ResFile mRes;
+    /* 0x36C */ dBgW mBgW;
+    /* 0x57C */ STATE_MGR_DECLARE(dAcOislandNusi_c);
 };
 
 #endif
