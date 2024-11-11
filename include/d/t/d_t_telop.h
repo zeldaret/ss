9525f8e9e5c20dccd8a9848fb6a3625f88c3e803
Elijah Thomas <42302100+elijah-thomas774@users.noreply.github.com>
2024-11-11 18:21:56 -0500

    d_t_telop OK (#100)

--- include/d/t/d_t_telop.h
+++ include/d/t/d_t_telop.h
@@ -1,14 +1,33 @@
 #ifndef D_T_TELOP_H
 #define D_T_TELOP_H
 
-#include "d/a/d_a_base.h"
+#include "d/flag/sceneflag_manager.h"
+#include "d/t/d_tg.h"
 
-class dTgTelop_c : public dAcBase_c {
+class dTgTelop_c : public dTg_c {
 public:
     dTgTelop_c() {}
     virtual ~dTgTelop_c() {}
 
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int draw() override;
+    virtual int actorExecute() override;
+
+    u16 getArea() const {
+        return params >> 8 & 0xFF;
+    }
+    u8 getSceneflag() const {
+        return params >> 16 & 0xFF;
+    }
+    u8 getTempflag() const {
+        return params >> 24 & 0xFF;
+    }
+
 private:
+    /* 0xFC */ u16 mArea;
+    /* 0xFD */ u8 mSceneflag;
+    /* 0xFF */ u8 mTempflag;
 };
 
 #endif
