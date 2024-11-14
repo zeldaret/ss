d3fc2b009a6409035173658b5391a3b082bb746e
robojumper <robojumper@gmail.com>
2024-11-14 13:41:53 +0100

    d_a_obj_water_shield almost OK, weak function order

--- include/d/a/obj/d_a_obj_water_shield.h
+++ include/d/a/obj/d_a_obj_water_shield.h
@@ -2,13 +2,31 @@
 #define D_A_OBJ_WATER_SHIELD_H
 
 #include "d/a/obj/d_a_obj_base.h"
+#include "d/col/bg/d_bg_w.h"
+#include "m/m3d/m_anmmatclr.h"
+#include "m/m3d/m_anmtexsrt.h"
+#include "m/m3d/m_smdl.h"
+#include "nw4r/g3d/g3d_resfile.h"
+#include "toBeSorted/stage_render_stuff.h"
 
 class dAcOwaterShield_c : public dAcObjBase_c {
 public:
-    dAcOwaterShield_c() {}
+    dAcOwaterShield_c() : mScnCallback(this) {}
     virtual ~dAcOwaterShield_c() {}
 
+    virtual bool createHeap() override;
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int actorExecute() override;
+    virtual int draw() override;
+
 private:
+    /* 0x330 */ nw4r::g3d::ResFile mRes;
+    /* 0x334 */ m3d::smdl_c mMdl;
+    /* 0x350 */ dBgW mBgW;
+    /* 0x560 */ m3d::anmTexSrt_c mAnmSrt;
+    /* 0x58C */ m3d::anmMatClr_c mAnmClr;
+    /* 0x5B8 */ dScnCallback_c mScnCallback;
 };
 
 #endif
--- src/REL/d/a/obj/d_a_obj_water_shield.cpp
+++ src/REL/d/a/obj/d_a_obj_water_shield.cpp
@@ -1,3 +1,53 @@
 #include "d/a/obj/d_a_obj_water_shield.h"
 
+#include "d/col/bg/d_bg_s.h"
+
 SPECIAL_ACTOR_PROFILE(OBJ_WATER_SHIELD, dAcOwaterShield_c, fProfile::OBJ_WATER_SHIELD, 0x218, 0, 0);
+
+bool dAcOwaterShield_c::createHeap() {
+    mRes = getOarcResFile("WaterWallF103");
+    nw4r::g3d::ResMdl mdl = mRes.GetResMdl("WaterWallF103");
+    TRY_CREATE(mMdl.create(mdl, &heap_allocator, 0x32C));
+    nw4r::g3d::ResAnmClr clr = mRes.GetResAnmClr("WaterWallF103");
+    TRY_CREATE(mAnmClr.create(mdl, clr, &heap_allocator, nullptr, 1));
+    nw4r::g3d::ResAnmTexSrt srt = mRes.GetResAnmTexSrt("WaterWallF103");
+    TRY_CREATE(mAnmSrt.create(mdl, srt, &heap_allocator, nullptr, 1));
+    cBgD_t *dzb = (cBgD_t *)getOarcFile("WaterWallF103", "dzb/WaterWallF103.dzb");
+    PLC *plc = (PLC *)getOarcFile("WaterWallF103", "dat/WaterWallF103.plc");
+    updateMatrix();
+    mMdl.setLocalMtx(mWorldMtx);
+    TRY_CREATE(!mBgW.Set(dzb, plc, cBgW::MOVE_BG_e, &mWorldMtx, &mScale));
+    mBgW.Lock();
+    return true;
+}
+
+int dAcOwaterShield_c::create() {
+    CREATE_ALLOCATOR(dAcOwaterShield_c);
+    dBgS::GetInstance()->Regist(&mBgW, this);
+    mScnCallback.attach(mMdl);
+    mMdl.setAnm(mAnmSrt);
+    mMdl.setAnm(mAnmClr);
+
+    mMdl.setPriorityDraw(0x1C, 0x9);
+
+    boundingBox.Set(mVec3_c(-12000.0f, -2000.0f, -14000.0f), mVec3_c(12000.0f, 11000.0f, 12000.0f));
+
+    return SUCCEEDED;
+}
+
+int dAcOwaterShield_c::doDelete() {
+    return SUCCEEDED;
+}
+
+int dAcOwaterShield_c::actorExecute() {
+    mAnmSrt.play();
+    mAnmClr.play();
+    return SUCCEEDED;
+}
+
+int dAcOwaterShield_c::draw() {
+    drawModelType1(&mMdl);
+    return SUCCEEDED;
+}
+
+// Weak function order issue IScnObjCallback
