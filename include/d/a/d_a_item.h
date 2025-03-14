255f1bb57fc949c382cf3a424a59f8f9c8171145
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2025-03-14 04:42:25 +0000

    Revert d_a_item spawnItem and spawnDrop param change

--- include/d/a/d_a_item.h
+++ include/d/a/d_a_item.h
@@ -44,8 +44,8 @@ public:
 
     static u32 createItemParams(u16 itemId, u32 subType, u32 unk, u8 unk2, u8, u8);
 
-    static void spawnItem(u16 item, u32 room, mVec3_c *pos, const mAng3_c &rot, u32 params, u32 arg);
-    static void spawnDrop(u16 item, u32 room, mVec3_c *pos, const mAng3_c &rot);
+    static void spawnItem(u16 item, u32 room, const mVec3_c &pos, const mAng3_c &rot, u32 params, u32 arg);
+    static void spawnDrop(u16 item, u32 room, const mVec3_c &pos, const mAng3_c &rot);
     static u32 checkFlag(u32 flag);
     void setItemPosition(const mVec3_c &);
     void getItemFromBWheelItem();
