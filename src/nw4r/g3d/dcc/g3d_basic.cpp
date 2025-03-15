674a673b1bf2f9a0c3808318bda3ffa077afb399
elijah-thomas774 <elijahthomas774@gmail.com>
2025-03-14 22:48:12 -0400

    g3d/dcc OK

--- src/nw4r/g3d/dcc/g3d_basic.cpp
+++ src/nw4r/g3d/dcc/g3d_basic.cpp
@@ -1,28 +1,49 @@
-#include <nw4r/g3d.h>
+#include "nw4r/g3d.h" // IWYU pragma: export
 
 namespace nw4r {
 namespace g3d {
 namespace detail {
 namespace dcc {
 
-u32 CalcWorldMtx_Basic(math::MTX34* pW, math::VEC3* pS, const math::MTX34* pW1,
-                       const math::VEC3* pS1, u32 attr,
-                       const ChrAnmResult* pResult) {
+bool CalcTexMtx_Basic(math::MTX34 *pMtx, bool set, const TexSrt &rSrt, TexSrt::Flag flag) {
+    // Extract S/R/T flags
+    u32 index = flag >> 1 & 0b111;
 
+    // Scale-one, no rotate/trans
+    if (index == 0b111) {
+        return false;
+    }
+
+    f32 sinR, cosR;
+    math::SinCosDeg(&sinR, &cosR, rSrt.R);
+
+    if (set) {
+        // clang-format off
+        pMtx->m[0][0] = rSrt.Su * cosR; pMtx->m[0][1] = -rSrt.Sv * sinR; pMtx->m[0][2] = 0.0f; pMtx->m[0][3] = rSrt.Tu;
+        pMtx->m[1][0] = rSrt.Su * sinR; pMtx->m[1][1] =  rSrt.Sv * cosR; pMtx->m[1][2] = 0.0f; pMtx->m[1][3] = rSrt.Tv;
+        pMtx->m[2][0] = 0.0f;           pMtx->m[2][1] =  0.0f;           pMtx->m[2][2] = 1.0f; pMtx->m[2][3] = 0.0f;
+        // clang-format on
+    } else {
+        // clang-format off
+        math::MTX34 m;
+        m[0][0] = rSrt.Su * cosR; m[0][1] = -rSrt.Sv * sinR; m[0][2] = 0.0f; m[0][3] = rSrt.Tu;
+        m[1][0] = rSrt.Su * sinR; m[1][1] =  rSrt.Sv * cosR; m[1][2] = 0.0f; m[1][3] = rSrt.Tv;
+        m[2][0] = 0.0f;           m[2][1] =  0.0f;           m[2][2] = 1.0f; m[2][3] = 0.0f;
+        // clang-format on
+        math::MTX34Mult(pMtx, &m, pMtx);
+    }
+
+    return true;
+}
+
+u32 CalcWorldMtx_Basic(
+    math::MTX34 *pW, math::VEC3 *pS, const math::MTX34 *pW1, const math::VEC3 *pS1, u32 attr,
+    const ChrAnmResult *pResult
+) {
     u32 flag = pResult->flags;
     u32 newAttr = attr;
 
-    if (flag & ChrAnmResult::FLAG_SCALE_ONE) {
-        newAttr = detail::WorldMtxAttr::AnmScaleOne(newAttr);
-        pS->x = pS->y = pS->z = 1.0f;
-    } else {
-        newAttr = detail::WorldMtxAttr::AnmNotScaleOne(newAttr);
-        *pS = pResult->s;
-    }
-
-    if ((flag & ChrAnmResult::FLAG_MTX_IDENT) ||
-        (flag & ChrAnmResult::FLAG_ROT_TRANS_ZERO)) {
-
+    if ((flag & ChrAnmResult::FLAG_MTX_IDENT) || (flag & ChrAnmResult::FLAG_ROT_TRANS_ZERO)) {
         if (detail::WorldMtxAttr::IsScaleOne(attr)) {
             math::MTX34Copy(pW, pW1);
         } else {
@@ -44,6 +65,14 @@ u32 CalcWorldMtx_Basic(math::MTX34* pW, math::VEC3* pS, const math::MTX34* pW1,
     } else {
         math::MTX34Scale(pW, pW1, pS1);
         math::MTX34Mult(pW, pW, &pResult->rt);
+    }
+
+    if (flag & ChrAnmResult::FLAG_SCALE_ONE) {
+        newAttr = detail::WorldMtxAttr::AnmScaleOne(newAttr);
+        pS->x = pS->y = pS->z = 1.0f;
+    } else {
+        newAttr = detail::WorldMtxAttr::AnmNotScaleOne(newAttr);
+        *pS = pResult->s;
     }
 
     if (flag & ChrAnmResult::FLAG_SCALE_UNIFORM) {
--- src/nw4r/g3d/dcc/g3d_maya.cpp
+++ src/nw4r/g3d/dcc/g3d_maya.cpp
@@ -1,4 +1,4 @@
-#include <nw4r/g3d.h>
+#include "nw4r/g3d.h" // IWYU pragma: export
 
 namespace nw4r {
 namespace g3d {
@@ -6,7 +6,7 @@ namespace detail {
 namespace dcc {
 namespace {
 
-void MakeTexSrtMtx_S(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_S(math::MTX34 *pMtx, const TexSrt &rSrt) {
     pMtx->_00 = rSrt.Su;
     pMtx->_01 = 0.0f;
     pMtx->_02 = 0.0f;
@@ -18,7 +18,7 @@ void MakeTexSrtMtx_S(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = 1.0f - rSrt.Sv;
 }
 
-void MakeTexSrtMtx_R(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_R(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
 
@@ -36,7 +36,7 @@ void MakeTexSrtMtx_R(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = cosPart + sinPart;
 }
 
-void MakeTexSrtMtx_T(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_T(math::MTX34 *pMtx, const TexSrt &rSrt) {
     pMtx->_00 = 1.0f;
     pMtx->_01 = 0.0f;
     pMtx->_02 = 0.0f;
@@ -48,7 +48,7 @@ void MakeTexSrtMtx_T(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = rSrt.Tv;
 }
 
-void MakeTexSrtMtx_SR(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_SR(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
 
@@ -68,7 +68,7 @@ void MakeTexSrtMtx_SR(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = 0.5f * (svsr - svcr - rSrt.Sv) + 1.0f;
 }
 
-void MakeTexSrtMtx_RT(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_RT(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
 
@@ -86,7 +86,7 @@ void MakeTexSrtMtx_RT(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = cosPart + sinPart + rSrt.Tv;
 }
 
-void MakeTexSrtMtx_ST(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_ST(math::MTX34 *pMtx, const TexSrt &rSrt) {
     pMtx->_00 = rSrt.Su;
     pMtx->_01 = 0.0f;
     pMtx->_02 = 0.0f;
@@ -98,7 +98,7 @@ void MakeTexSrtMtx_ST(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = rSrt.Sv * (rSrt.Tv - 1.0f) + 1.0f;
 }
 
-void MakeTexSrtMtx_SRT(math::MTX34* pMtx, const TexSrt& rSrt) {
+void MakeTexSrtMtx_SRT(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
 
@@ -116,7 +116,7 @@ void MakeTexSrtMtx_SRT(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = rSrt.Sv * (cosPart + sinPart + rSrt.Tv) + 1.0f;
 }
 
-void ProductTexSrtMtx_S(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_S(math::MTX34 *pMtx, const TexSrt &rSrt) {
     pMtx->_00 *= rSrt.Su;
     pMtx->_01 *= rSrt.Su;
     pMtx->_02 *= rSrt.Su;
@@ -128,7 +128,7 @@ void ProductTexSrtMtx_S(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = 1.0f + pMtx->_13 * rSrt.Sv - rSrt.Sv;
 }
 
-void ProductTexSrtMtx_R(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_R(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     f32 _0x, _1x;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
@@ -157,12 +157,12 @@ void ProductTexSrtMtx_R(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = _1x;
 }
 
-void ProductTexSrtMtx_T(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_T(math::MTX34 *pMtx, const TexSrt &rSrt) {
     pMtx->_03 += -rSrt.Tu;
     pMtx->_13 += rSrt.Tv;
 }
 
-void ProductTexSrtMtx_SR(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_SR(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     f32 _0x, _1x;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
@@ -201,7 +201,7 @@ void ProductTexSrtMtx_SR(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = _1x;
 }
 
-void ProductTexSrtMtx_RT(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_RT(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     f32 _0x, _1x;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
@@ -230,7 +230,7 @@ void ProductTexSrtMtx_RT(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = _1x;
 }
 
-void ProductTexSrtMtx_ST(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_ST(math::MTX34 *pMtx, const TexSrt &rSrt) {
     pMtx->_00 *= rSrt.Su;
     pMtx->_01 *= rSrt.Su;
     pMtx->_02 *= rSrt.Su;
@@ -242,7 +242,7 @@ void ProductTexSrtMtx_ST(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_13 = rSrt.Sv * (pMtx->_13 + rSrt.Tv - 1.0f) + 1.0f;
 }
 
-void ProductTexSrtMtx_SRT(math::MTX34* pMtx, const TexSrt& rSrt) {
+void ProductTexSrtMtx_SRT(math::MTX34 *pMtx, const TexSrt &rSrt) {
     f32 sinR, cosR;
     f32 _0x, _1x;
     math::SinCosDeg(&sinR, &cosR, rSrt.R);
@@ -267,11 +267,9 @@ void ProductTexSrtMtx_SRT(math::MTX34* pMtx, const TexSrt& rSrt) {
     pMtx->_02 = _0x;
     pMtx->_12 = _1x;
 
-    _0x = sucr * (pMtx->_03 - 0.5f) + susr * (pMtx->_13 - 0.5f) +
-          (0.5f - rSrt.Tu) * rSrt.Su;
+    _0x = sucr * (pMtx->_03 - 0.5f) + susr * (pMtx->_13 - 0.5f) + (0.5f - rSrt.Tu) * rSrt.Su;
 
-    _1x = -svsr * (0.5f + pMtx->_03) + svcr * (pMtx->_13 - 0.5f) -
-          (0.5f - rSrt.Tv) * rSrt.Sv + 1.0f;
+    _1x = -svsr * (0.5f + pMtx->_03) + svcr * (pMtx->_13 - 0.5f) - (0.5f - rSrt.Tv) * rSrt.Sv + 1.0f;
 
     pMtx->_03 = _0x;
     pMtx->_13 = _1x;
@@ -279,10 +277,9 @@ void ProductTexSrtMtx_SRT(math::MTX34* pMtx, const TexSrt& rSrt) {
 
 } // namespace
 
-typedef void (*CalcTexMtxFunc)(math::MTX34* pMtx, const TexSrt& rSrt);
+typedef void (*CalcTexMtxFunc)(math::MTX34 *pMtx, const TexSrt &rSrt);
 
-bool CalcTexMtx_Maya(math::MTX34* pMtx, bool set, const TexSrt& rSrt,
-                     TexSrt::Flag flag) {
+bool CalcTexMtx_Maya(math::MTX34 *pMtx, bool set, const TexSrt &rSrt, TexSrt::Flag flag) {
     // Extract S/R/T flags
     u32 index = flag >> 1 & 0b111;
 
@@ -325,11 +322,38 @@ bool CalcTexMtx_Maya(math::MTX34* pMtx, bool set, const TexSrt& rSrt,
     return true;
 }
 
-u32 CalcWorldMtx_Maya_SSC_Apply(math::MTX34* pW, math::VEC3* pS,
-                                const math::MTX34* pW1, const math::VEC3* pS1,
-                                u32 attr, const ChrAnmResult* pResult) {
+u32 CalcWorldMtx_Maya_SSC_Apply(
+    math::MTX34 *pW, math::VEC3 *pS, const math::MTX34 *pW1, const math::VEC3 *pS1, u32 attr,
+    const ChrAnmResult *pResult
+) {
     u32 flag = pResult->flags;
     u32 newAttr = attr;
+
+    if ((flag & ChrAnmResult::FLAG_MTX_IDENT) || (flag & ChrAnmResult::FLAG_ROT_TRANS_ZERO)) {
+        math::MTX34Copy(pW, pW1);
+    } else if (flag & ChrAnmResult::FLAG_ROT_ZERO) {
+        if (detail::WorldMtxAttr::IsScaleOne(attr)) {
+            math::VEC3 trans(pResult->rt._03, pResult->rt._13, pResult->rt._23);
+
+            math::MTX34Trans(pW, pW1, &trans);
+        } else {
+            math::VEC3 trans(pS1->x * pResult->rt._03, pS1->y * pResult->rt._13, pS1->z * pResult->rt._23);
+
+            math::MTX34Trans(pW, pW1, &trans);
+        }
+    } else if (detail::WorldMtxAttr::IsScaleOne(attr)) {
+        math::MTX34Mult(pW, pW1, &pResult->rt);
+    } else {
+        math::MTX34 m;
+
+        math::MTX34Copy(&m, &pResult->rt);
+
+        m._03 *= pS1->x;
+        m._13 *= pS1->y;
+        m._23 *= pS1->z;
+
+        math::MTX34Mult(pW, pW1, &m);
+    }
 
     if (flag & ChrAnmResult::FLAG_SCALE_ONE) {
         newAttr = detail::WorldMtxAttr::AnmScaleOne(newAttr);
@@ -337,34 +361,6 @@ u32 CalcWorldMtx_Maya_SSC_Apply(math::MTX34* pW, math::VEC3* pS,
     } else {
         newAttr = detail::WorldMtxAttr::AnmNotScaleOne(newAttr);
         *pS = pResult->s;
-    }
-
-    if ((flag & ChrAnmResult::FLAG_MTX_IDENT) ||
-        (flag & ChrAnmResult::FLAG_ROT_TRANS_ZERO)) {
-
-        math::MTX34Copy(pW, pW1);
-    } else if (flag & ChrAnmResult::FLAG_ROT_ZERO) {
-
-        if (detail::WorldMtxAttr::IsScaleOne(attr)) {
-            math::VEC3 trans(pResult->rt._03, pResult->rt._13, pResult->rt._23);
-
-            math::MTX34Trans(pW, pW1, &trans);
-        } else {
-            math::VEC3 trans(pS1->x * pResult->rt._03, pS1->y * pResult->rt._13,
-                             pS1->z * pResult->rt._23);
-
-            math::MTX34Trans(pW, pW1, &trans);
-        }
-    } else if (detail::WorldMtxAttr::IsScaleOne(attr)) {
-        math::MTX34Mult(pW, pW1, &pResult->rt);
-    } else {
-        math::MTX34Copy(pW, &pResult->rt);
-
-        pW->_03 *= pS1->x;
-        pW->_13 *= pS1->y;
-        pW->_23 *= pS1->z;
-
-        math::MTX34Mult(pW, pW1, pW);
     }
 
     if (flag & ChrAnmResult::FLAG_SCALE_UNIFORM) {
