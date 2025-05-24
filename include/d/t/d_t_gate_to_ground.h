1a2455757a6439018d929775ffc0cdeae3e8749d
Orstedra <daeusosenna@gmail.com>
2025-05-24 14:28:22 +0200

    Initial commit, function defs

--- include/d/t/d_t_gate_to_ground.h
+++ include/d/t/d_t_gate_to_ground.h
@@ -8,6 +8,12 @@ public:
     dTgGateToGround_c() {}
     virtual ~dTgGateToGround_c() {}
 
+    virtual int create() override;
+    virtual int doDelete() override; // fn_205_150
+    virtual int actorExecute() override;
+    virtual int actorExecuteInEvent() override;
+    virtual int draw() override; // fn_205_2F0
+
 private:
 };
 
--- src/REL/d/t/d_t_gate_to_ground.cpp
+++ src/REL/d/t/d_t_gate_to_ground.cpp
@@ -1,3 +1,20 @@
 #include "d/t/d_t_gate_to_ground.h"
 
+#include "f/f_base.h"
+
+
 SPECIAL_ACTOR_PROFILE(GATE2GND_TAG, dTgGateToGround_c, fProfile::GATE2GND_TAG, 0x22, 0, 0);
+
+int dTgGateToGround_c::create() {}
+
+int dTgGateToGround_c::doDelete() {
+    return SUCCEEDED;
+}
+
+int dTgGateToGround_c::actorExecute() {}
+
+int dTgGateToGround_c::actorExecuteInEvent() {}
+
+int dTgGateToGround_c::draw() {
+    return SUCCEEDED;
+}
