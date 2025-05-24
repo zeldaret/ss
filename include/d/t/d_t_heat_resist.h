b8b2375891b14698cad4a3e167eee2b669f538ca
Orstedra <115906093+Orstedra@users.noreply.github.com>
2025-05-24 04:22:49 +0200

    d_t_heat_resist OK (#173)
    * d_t_heat_resist OK

--- include/d/t/d_t_heat_resist.h
+++ include/d/t/d_t_heat_resist.h
@@ -1,14 +1,23 @@
 #ifndef D_T_HEAT_RESIST_H
 #define D_T_HEAT_RESIST_H
 
-#include "d/a/d_a_base.h"
+#include "common.h"
+#include "d/t/d_tg.h"
+#include "m/m_mtx.h"
 
-class dTgHeatResist_c : public dAcBase_c {
+class dTgHeatResist_c : public dTg_c {
 public:
     dTgHeatResist_c() {}
     virtual ~dTgHeatResist_c() {}
 
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int draw() override;
+    virtual int actorExecute() override;
+
 private:
+    /* 0x0FC */ mMtx_c matrix;
+    /* 0x12C */ u8 mInverted;
 };
 
 #endif
