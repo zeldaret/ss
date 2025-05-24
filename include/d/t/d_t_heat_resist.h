2075c93c05b00594a00dc17b7c7f438b249facae
Orstedra <daeusosenna@gmail.com>
2025-05-24 03:43:35 +0200

    d_t_heat_resist OK

--- include/d/t/d_t_heat_resist.h
+++ include/d/t/d_t_heat_resist.h
@@ -1,14 +1,23 @@
 #ifndef D_T_HEAT_RESIST_H
 #define D_T_HEAT_RESIST_H
 
-#include "d/a/d_a_base.h"
+#include "common.h"
+#include "d/t/d_tg.h"
+#include "m/m_mtx.h"
 
-class dTgHeatResist_c : public dAcBase_c {
+class dTgHeatResist_c : public dTg_c {
 public:
     dTgHeatResist_c() {}
     virtual ~dTgHeatResist_c() {}
 
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int draw() override;
+    virtual int actorExecute() override;
+
 private:
+    mMtx_c matrix;
+    u8 mInverted;
 };
 
 #endif
--- src/REL/d/t/d_t_heat_resist.cpp
+++ src/REL/d/t/d_t_heat_resist.cpp
@@ -1,3 +1,37 @@
 #include "d/t/d_t_heat_resist.h"
 
+#include "d/a/d_a_player.h"
+#include "f/f_base.h"
+#include "toBeSorted/area_math.h"
+
 SPECIAL_ACTOR_PROFILE(TAG_HEAT_RESIST, dTgHeatResist_c, fProfile::TAG_HEAT_RESIST, 0x223, 0, 4);
+
+int dTgHeatResist_c::create() {
+    mInverted = static_cast<u8>(params & 0x3);
+
+    matrixCreateFromPosRotYScale(matrix, position, rotation.y, mScale, nullptr, 0.0f);
+    return SUCCEEDED;
+}
+
+int dTgHeatResist_c::doDelete() {
+    return SUCCEEDED;
+}
+
+int dTgHeatResist_c::actorExecute() {
+    dAcPy_c *player = dAcPy_c::LINK;
+
+    if (mInverted == 1) {
+        if (!checkIfVec3fInMatrix(matrix, player->position)) {
+            player->onForceOrPreventActionFlags(0x8000000);
+        }
+    } else {
+        if (checkIfVec3fInMatrix(matrix, player->position)) {
+            player->onForceOrPreventActionFlags(0x8000000);
+        }
+    }
+    return SUCCEEDED;
+}
+
+int dTgHeatResist_c::draw() {
+    return SUCCEEDED;
+}
