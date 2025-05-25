306f620a9f8d5a54bb4d2a54e06e5aec1d6403e7
robojumper <robojumper@gmail.com>
2025-05-25 22:51:08 +0200

    snd_MidiSeqPlayer OK

--- configure.py
+++ configure.py
@@ -1039,7 +1039,7 @@ config.libs = [
             Object(Matching, "nw4r/snd/snd_InstancePool.cpp"),
             Object(Matching, "nw4r/snd/snd_Lfo.cpp"),
             Object(Matching, "nw4r/snd/snd_MemorySoundArchive.cpp"),
-            Object(NonMatching, "nw4r/snd/snd_MidiSeqPlayer.cpp"),
+            Object(Matching, "nw4r/snd/snd_MidiSeqPlayer.cpp"),
             Object(NonMatching, "nw4r/snd/snd_MmlParser.cpp"),
             Object(NonMatching, "nw4r/snd/snd_MmlSeqTrack.cpp"),
             Object(NonMatching, "nw4r/snd/snd_MmlSeqTrackAllocator.cpp"),
