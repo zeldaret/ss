8ed188fb25d36e2da0a38634f954c0cba447e533
robojumper <robojumper@gmail.com>
2025-05-25 22:44:44 +0200

    snd_Lfo OK

--- configure.py
+++ configure.py
@@ -1037,7 +1037,7 @@ config.libs = [
             Object(NonMatching, "nw4r/snd/snd_FxReverbStd.cpp"),
             Object(NonMatching, "nw4r/snd/snd_FxReverbStdDpl2.cpp"),
             Object(Matching, "nw4r/snd/snd_InstancePool.cpp"),
-            Object(NonMatching, "nw4r/snd/snd_Lfo.cpp"),
+            Object(Matching, "nw4r/snd/snd_Lfo.cpp"),
             Object(NonMatching, "nw4r/snd/snd_MemorySoundArchive.cpp"),
             Object(NonMatching, "nw4r/snd/snd_MidiSeqPlayer.cpp"),
             Object(NonMatching, "nw4r/snd/snd_MmlParser.cpp"),
