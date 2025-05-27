42a26121ef09cce9ee336970d8b7dc705ce258f9
Orstedra <daeusosenna@gmail.com>
2025-05-27 20:04:07 +0200

    Initial commit

--- include/d/a/obj/d_a_obj_girahim_foot.h
+++ include/d/a/obj/d_a_obj_girahim_foot.h
@@ -2,6 +2,8 @@
 #define D_A_OBJ_GIRAHIM_FOOT_H
 
 #include "d/a/obj/d_a_obj_base.h"
+#include "m/m3d/m_smdl.h"
+#include "nw4r/g3d/res/g3d_resfile.h"
 #include "s/s_State.hpp"
 #include "s/s_StateMgr.hpp"
 
@@ -10,10 +12,19 @@ public:
     dAcOgirahimFoot_c() : mStateMgr(*this, sStateID::null) {}
     virtual ~dAcOgirahimFoot_c() {}
 
+    virtual int create() override;       // fn_425_4C0
+    virtual int doDelete() override;     // fn_425_5C0
+    virtual int draw() override;         // fn_425_610
+    virtual bool createHeap() override;  // fn_425_440
+    virtual int actorExecute() override; // fn_425_5D0
+
     STATE_FUNC_DECLARE(dAcOgirahimFoot_c, Wait);
     STATE_FUNC_DECLARE(dAcOgirahimFoot_c, Appear);
 
 private:
+    nw4r::g3d::ResFile mRes;
+
+    m3d::smdl_c mMdl;
     /* 0x??? */ STATE_MGR_DECLARE(dAcOgirahimFoot_c);
 };
 
--- src/REL/d/a/obj/d_a_obj_girahim_foot.cpp
+++ src/REL/d/a/obj/d_a_obj_girahim_foot.cpp
@@ -1,9 +1,37 @@
 #include "d/a/obj/d_a_obj_girahim_foot.h"
 
+#include "d/a/obj/d_a_obj_base.h"
+#include "f/f_base.h"
+#include "nw4r/g3d/res/g3d_resfile.h"
+
 SPECIAL_ACTOR_PROFILE(OBJ_GIRAHIM_FOOT, dAcOgirahimFoot_c, fProfile::OBJ_GIRAHIM_FOOT, 0x210, 0, 6);
 
 STATE_DEFINE(dAcOgirahimFoot_c, Wait);
 STATE_DEFINE(dAcOgirahimFoot_c, Appear);
+
+int dAcOgirahimFoot_c::create() {
+    return SUCCEEDED;
+}
+
+int dAcOgirahimFoot_c::doDelete() {
+    return SUCCEEDED;
+};
+
+int dAcOgirahimFoot_c::draw() {
+    return SUCCEEDED;
+}
+
+bool dAcOgirahimFoot_c::createHeap() {
+    mRes = nw4r::g3d::ResFile(getOarcResFile("Girahim_Foot"));
+
+    nw4r::g3d::ResMdl resMdl = mRes.GetResMdl("Girahim_Foot");
+
+    TRY_CREATE(mMdl.create(resMdl, &heap_allocator, 0x120, 1, nullptr));
+}
+
+int dAcOgirahimFoot_c::actorExecute() {
+    return SUCCEEDED;
+}
 
 void dAcOgirahimFoot_c::initializeState_Wait() {}
 void dAcOgirahimFoot_c::executeState_Wait() {}
