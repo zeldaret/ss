6b9aeb9a9ae29154d55150ea25361b826cbc18f6
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2025-05-27 05:26:02 +0100

    d_a_obj_distant_ship OK

--- include/d/a/obj/d_a_obj_distant_ship.h
+++ include/d/a/obj/d_a_obj_distant_ship.h
@@ -2,13 +2,25 @@
 #define D_A_OBJ_DISTANT_SHIP_H
 
 #include "d/a/obj/d_a_obj_base.h"
+#include "m/m3d/m_smdl.h"
+#include "nw4r/g3d/res/g3d_resfile.h"
 
 class dAcOdistantShip_c : public dAcObjBase_c {
 public:
     dAcOdistantShip_c() {}
     virtual ~dAcOdistantShip_c() {}
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int draw() override;
+    virtual bool createHeap() override;
+    virtual int actorExecute() override;
+    char *getModelName();
 
 private:
+    /* 0x330 */ nw4r::g3d::ResFile mBrres;
+    /* 0x334 */ m3d::smdl_c mModel;
+    /* 0x350 */ u8 mSubtype;
+    /* 0x351 */ bool mIsVisible;
 };
 
 #endif
--- src/REL/d/a/obj/d_a_obj_distant_ship.cpp
+++ src/REL/d/a/obj/d_a_obj_distant_ship.cpp
@@ -1,3 +1,61 @@
 #include "d/a/obj/d_a_obj_distant_ship.h"
 
+#include "common.h"
+#include "d/a/obj/d_a_obj_base.h"
+#include "d/flag/sceneflag_manager.h"
+#include "f/f_base.h"
+#include "nw4r/g3d/res/g3d_resfile.h"
+#include "toBeSorted/arc_managers/current_stage_arc_manager.h"
+
 SPECIAL_ACTOR_PROFILE(OBJ_DIS_SHIP, dAcOdistantShip_c, fProfile::OBJ_DIS_SHIP, 0x143, 0, 0);
+
+bool dAcOdistantShip_c::createHeap() {
+    char *modelName = getModelName();
+    mBrres = (nw4r::g3d::ResFile)CurrentStageArcManager::GetInstance()->getData("g3d/stage.brres");
+    TRY_CREATE(mModel.create(mBrres.GetResMdl(modelName), &heap_allocator, 0x120, 1, 0));
+    return SUCCEEDED;
+}
+
+int dAcOdistantShip_c::create() {
+    // Sure, why not
+    mIsVisible = false;
+    u32 p = params;
+    u16 r = (s32)roomid;
+    mSubtype = (p >> 8) & 0xF;
+
+    bool flag = SceneflagManager::sInstance->checkFlag(r, p & 0xFF);
+    if (flag) {
+        mIsVisible = true;
+    }
+
+    if (mIsVisible) {
+        CREATE_ALLOCATOR(dAcOdistantShip_c);
+        updateMatrix();
+        mModel.setLocalMtx(mWorldMtx);
+        boundingBox.Set(mVec3_c(-0.0f, -0.0f, -0.0f), mVec3_c(0.0f, 0.0f, 0.0f));
+    }
+    return SUCCEEDED;
+}
+
+int dAcOdistantShip_c::doDelete() {
+    return SUCCEEDED;
+}
+
+int dAcOdistantShip_c::actorExecute() {
+    return SUCCEEDED;
+}
+
+int dAcOdistantShip_c::draw() {
+    if (mIsVisible) {
+        drawModelType1(&mModel);
+    }
+    return SUCCEEDED;
+}
+
+char *dAcOdistantShip_c::getModelName() {
+    switch (mSubtype) {
+        case 0:  return "StageF301_D300";
+        case 1:  return "StageF301_3_D300";
+        default: return nullptr;
+    }
+}
