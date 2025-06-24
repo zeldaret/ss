52a0c6ef639ca8e8e1a52042420a9309b9db1f33
robojumper <robojumper@gmail.com>
2025-06-24 22:15:25 +0200

    Another two obj source files

---
+++ include/d/snd/d_snd_source_obj_clef.h
@@ -0,0 +1,14 @@
+#ifndef D_SND_SOURCE_OBJ_CLEF_H
+#define D_SND_SOURCE_OBJ_CLEF_H
+
+#include "d/snd/d_snd_source_obj.h"
+
+class dSndSourceObjClef_c : public dSndSourceObj_c {
+public:
+    dSndSourceObjClef_c(u8 sourceType, dAcBase_c *ac, const char *name, dSndSourceGroup_c *pOwnerGroup)
+        : dSndSourceObj_c(sourceType, ac, name, pOwnerGroup) {}
+
+    bool startTadtoneSound(u32 groupIdx, u32 noteIdx);
+};
+
+#endif
