e97bb9a75e0e70e8ff173df95525b2d4948d4799
robojumper <robojumper@gmail.com>
2025-07-05 12:27:21 +0200

    d_snd_id_mappers OK

--- include/d/snd/d_snd_id_mappers.h
+++ include/d/snd/d_snd_id_mappers.h
@@ -2,6 +2,11 @@
 #define D_SND_ID_MAPPERS_H
 
 #include "common.h"
+#include "d/snd/d_snd_types.h"
+
+u32 getGrpId(dSndSourceGroup_c *pGroup);
+u32 getBnkSeId(dSndSourceGroup_c *pGroup);
+u32 getSeId(dSndSourceGroup_c *pGroup);
 
 // TODO move somewhere else, or maybe remove entirely
 // could plausibly belong in d/col/bg
