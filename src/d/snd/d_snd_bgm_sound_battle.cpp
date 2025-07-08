f5a39b632def69178515b978d369c700ac40ea74
robojumper <robojumper@gmail.com>
2025-07-09 00:30:48 +0200

    d_snd_bgm_sound_battle OK

--- src/d/snd/d_snd_bgm_sound_battle.cpp
+++ src/d/snd/d_snd_bgm_sound_battle.cpp
@@ -5,14 +5,15 @@
 #include "d/snd/d_snd_bgm_sound.h"
 #include "d/snd/d_snd_mgr.h"
 #include "d/snd/d_snd_wzsound.h"
+#include "nw4r/snd/snd_SeqSoundHandle.h"
 
 dSndBgmBattleSound_c::dSndBgmBattleSound_c()
-    : mpBgmBattleConfig(nullptr), mMuteApplyStateMask(0), mTickRelated(0), field_0x190(0), field_0x191(1) {}
+    : mpBgmBattleConfig(nullptr), mMuteGroupActiveMask(0), mPrevTick(0), field_0x190(0), field_0x191(1) {}
 
 void dSndBgmBattleSound_c::cancel() {
     dSndBgmSound_c::cancel();
-    mMuteApplyStateMask = 0;
-    mTickRelated = 0;
+    mMuteGroupActiveMask = 0;
+    mPrevTick = 0;
     field_0x190 = 0;
     field_0x191 = 1;
 }
@@ -42,5 +43,56 @@ void dSndBgmBattleSound_c::fadeIn(u32 id, s32 fadeFrames) {
 
     if (GetId() != BGM_BATTLE2) {
         field_0x191 = 0;
+    }
+}
+
+void dSndBgmBattleSound_c::setTrackGroupMuted(u32 groupId) {
+    mMuteGroupActiveMask |= (1 << groupId);
+}
+
+void dSndBgmBattleSound_c::setTrackGroupUnmuted(u32 groupId) {
+    mMuteGroupActiveMask &= ~(1 << groupId);
+}
+
+bool dSndBgmBattleSound_c::startMainBattleLoop() {
+    if (isPlaying() && readSeqTrackVariable(0) != 1) {
+        // This seq variable allows the seq sound to jump from intro to main loop
+        writeSeqTrackVariable(0, 1);
+        mpSeqConfig = dSndBgmSeqConfig::getConfig(GetId(), 1);
+        return true;
+    }
+
+    return false;
+}
+
+void dSndBgmBattleSound_c::calcSeqPlaySamplePosition() {
+    if (!isSeqSound()) {
+        return;
+    }
+
+    nw4r::snd::SeqSoundHandle handle(this);
+    s32 tick = handle.GetTick();
+    s32 tickDiff = tick - mPrevTick;
+
+    if (tickDiff <= 0) {
+        return;
+    }
+
+    mPrevTick = tick;
+    s32 timeBase = mpSeqConfig != nullptr ? mpSeqConfig->mTimebase : 0x180;
+    mBgmVar3 = readSeqTrackVariable(3);
+    if (mBgmVar3 >= 0) {
+        // Var >= 0 - seq itself controls play position
+        mSeqPlaySamplePosition = tick % mSeqTimebase;
+        mSeqPlaySamplePosition += mSeqTimebase * mBgmVar3;
+    } else if (mpSeqConfig != nullptr) {
+        mSeqPlaySamplePosition += tickDiff;
+        if (mSeqPlaySamplePosition < (mpSeqConfig->mLoopEnd + 1) * timeBase) {
+            // not looped yet, no adjustment needed
+            return;
+        }
+        // looped, simple adjustment - simplified compared to dSndBgmSound_c which always calculates
+        // mSeqPlaySamplePosition freshly. This function instead uses the difference
+        mSeqPlaySamplePosition -= (timeBase * (mpSeqConfig->mLoopEnd - mpSeqConfig->mLoopStart));
     }
 }
