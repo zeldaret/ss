5b6f0df1028c98799bc4c9e39c78e41ab9866512
Asiern <git@asiern.com>
2025-11-21 22:55:18 +0100

    feat: refactor harp actor to new actor base

--- include/d/t/d_t_harp.h
+++ include/d/t/d_t_harp.h
@@ -2,10 +2,11 @@
 #define D_T_HARP_H
 
 #include "common.h"
-#include "d/a/d_a_base.h"
+#include "d/snd/d_snd_source_harp_related.h"
+#include "d/t/d_tg.h"
 #include "s/s_State.hpp"
 
-class dTgHarp_c : public dAcBase_c {
+class dTgHarp_c : public dTg_c {
 public:
     dTgHarp_c() : mStateMgr(*this, sStateID::null) {}
     virtual ~dTgHarp_c() {}
@@ -14,9 +15,19 @@ public:
 
 private:
     /* 0x??? */ STATE_MGR_DECLARE(dTgHarp_c);
-    s16 fn_536_420();
-    s16 fn_536_530();
-    s16 fn_536_540();
+    /* 0x138 */ byte_t field_0x138;
+    /* 0x139 */ byte_t field_0x139;
+    /* 0x13A */ s16 field_0x13a;
+    /* 0x13C */ u16 mTimer_0x13c;
+    /* 0x13E */ byte_t sceneflag_0x13e;
+
+    virtual bool createHeap() override;  // fn_536_420
+    virtual int create() override;       // fn_536_430
+    virtual int doDelete() override;     // fn_536_530
+    virtual int actorExecute() override; // fn_536_540
+    virtual int draw() override;         // fn_536_580
+    void fn_536_6C0();                   // fn_536_6C0
+    void fn_536_720(dSndSourceHarpRelated_c *soundSource);
 };
 
 #endif
--- src/REL/d/t/d_t_harp.cpp
+++ src/REL/d/t/d_t_harp.cpp
@@ -1,22 +1,113 @@
 #include "d/t/d_t_harp.h"
 
+#include "common.h"
+#include "d/a/d_a_item.h"
+#include "d/a/d_a_itembase.h"
+#include "d/flag/sceneflag_manager.h"
+#include "d/snd/d_snd_source_harp_related.h"
+#include "d/snd/d_snd_source_if.h"
+#include "s/s_Math.h"
+
 SPECIAL_ACTOR_PROFILE(HARP_TAG, dTgHarp_c, fProfile::HARP_TAG, 0x1A4, 0, 0);
 
 STATE_DEFINE(dTgHarp_c, Wait);
 
-s16 dTgHarp_c::fn_536_420() {
+bool dTgHarp_c::createHeap() {
+    return true;
+}
+
+int dTgHarp_c::create() {
+    u32 tmp = (mParams >> 8) & 0xF;
+    if (tmp != 0xF) {
+        field_0x13a = tmp * 0x1E;
+    } else {
+        field_0x13a = 0x96;
+    }
+
+    if (initAllocatorWork1Heap(-1, const_cast<char *>("dTgHarp_c::m_allocator"), 0x20) == 0) {
+        return 2;
+    }
+
+    fn_536_6C0();
+    mTimer_0x13c = field_0x13a;
+    byte_t lowByte = mParams & 0xFF;
+    sceneflag_0x13e = lowByte;
+
+    if (lowByte < 0xFF) {
+        bool flagSet = SceneflagManager::sInstance->checkFlag(mRoomID, mParams & 0xFF);
+
+        if (flagSet) {
+            return 2;
+        }
+    }
+    mStateMgr.changeState(StateID_Wait);
     return 1;
 }
 
-s16 dTgHarp_c::fn_536_530() {
+int dTgHarp_c::doDelete() {
     return 1;
 }
 
-s16 dTgHarp_c::fn_536_540() {
+int dTgHarp_c::actorExecute() {
     mStateMgr.executeState();
     return 1;
 }
 
+int dTgHarp_c::draw() {
+    return 1;
+}
+
+void dTgHarp_c::fn_536_6C0() {
+    // TODO: soundSource should be of type dSndSourceHarpRelated_c*
+    dSoundSourceIf_c *soundSource = getSoundSource();
+    if (soundSource != nullptr) {
+        soundSource->offFlag(2);
+        // fn_536_720(soundSource);
+    }
+}
+
+void dTgHarp_c::fn_536_720(dSndSourceHarpRelated_c *soundSource) {
+    if (field_0x138 == 0 && dAcItem_c::checkFlag(ITEM_GODDESS_HARP)) {
+        soundSource->onFlag(2);
+        soundSource->d_s_harp_vt_0x1F0(); // Offset to 0x1F0 is off by 4 bytes
+        field_0x138 = 1;
+    }
+}
+
+// Actual implementation and definition is located in d/snd/d_snd_source_harp_related.cpp
+bool harpSoundRelated() {
+    return true;
+}
+
 void dTgHarp_c::initializeState_Wait() {}
-void dTgHarp_c::executeState_Wait() {}
+
+void dTgHarp_c::executeState_Wait() {
+    // TODO: soundSource should be of type dSndSourceHarpRelated_c*
+    dSoundSourceIf_c *soundSource = getSoundSource();
+    if (soundSource != nullptr) {
+        return;
+    }
+
+    // fn_536_720(soundSource);
+
+    if (isWithinPlayerRadius(mScale.x)) {
+        // TODO: call harpSoundRelated with from d/snd/d_snd_source_harp_related.cpp
+        if (harpSoundRelated() && !sLib::calcTimer(&mTimer_0x13c)) {
+            if (sceneflag_0x13e < 0xff) {
+                bool flagStatus = SceneflagManager::sInstance->checkFlag(mRoomID, sceneflag_0x13e);
+
+                if (!flagStatus) {
+                    SceneflagManager::sInstance->setFlag(mRoomID, sceneflag_0x13e);
+                }
+            }
+
+            deleteRequest();
+        }
+
+    } else {
+        field_0x13a = 0x96;
+        mTimer_0x13c = 0x96;
+    }
+}
+
 void dTgHarp_c::finalizeState_Wait() {}
