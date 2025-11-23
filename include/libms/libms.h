2b0e4985ff3f9e75ade31c29d11eb62a56671241
elijah-thomas774 <elijahthomas774@gmail.com>
2025-11-22 20:49:58 -0500

    Libms matching: Thanks Troy from gc/wii decomp server Reference:  https://github.com/Trippixyz/LibMessageStudio/blob/ceb14124dbf037627a3c40e2bb9c87725baf3cbb/src/libms.c

--- include/libms/libms.h
+++ include/libms/libms.h
@@ -20,8 +20,8 @@ void LMS_SetMemFuncs(void *(*alloc)(size_t size), void (*free)(void *ptr));
 // internal
 void *LMSi_Malloc(size_t size);
 void LMSi_Free(void *ptr);
-int LMSi_MemCmp(const char *p1, const char *p2, int n);
-void LMSi_MemCopy(char *p1, const char *p2, int n);
+int LMSi_MemCmp(const void *p1, const void *p2, int n);
+void LMSi_MemCopy(void *p1, const void *p2, int n);
 
 #ifdef __cplusplus
 }
