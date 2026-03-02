bd5f80c0f4214475ac546aa55b41f6e5ac707f39
MaxMotis <mr.maxmotis@gmail.com>
2026-03-02 11:10:39 +0100

    d_t_event OK
    Now also formatted d_t_event.h with clang-format

--- include/d/t/d_t_event.h
+++ include/d/t/d_t_event.h
@@ -5,29 +5,27 @@
 #include "m/m_mtx.h"
 #include "toBeSorted/actor_event.h"
 
-class dTgEvent_c : public dTg_c { 
+class dTgEvent_c : public dTg_c {
 public:
-	dTgEvent_c() : mActorEvent(*this, nullptr) {}
-	virtual ~dTgEvent_c() {}
+    dTgEvent_c() : mActorEvent(*this, nullptr) {}
+    virtual ~dTgEvent_c() {}
 
-	virtual int create() override;
-	virtual int doDelete() override;
+    virtual int create() override;
+    virtual int doDelete() override;
     virtual int actorExecute() override;
     virtual int actorExecuteInEvent() override;
-	virtual int draw() override;
+    virtual int draw() override;
 
-	bool checkUncommittedFlag(u16 flag);
-	bool isValidFlag(u16 flag);
+    bool checkUncommittedFlag(u16 flag);
+    bool isValidFlag(u16 flag);
 
 private:
-
-	/* 0x0FC */ ActorEventRelated mActorEvent;
-	/* 0x14C */ mMtx_c mMatrix;
-	/* 0x17C */ u32 mEventId;
-	/* 0x180 */ u32 mSetSceneflag;
-	/* 0x184 */ u32 mTrigSceneflag;
-	/* 0x188 */ u32 mSubtype;
-	/* 0x18C */ u32 mUnkParamFlag;
-
+    /* 0x0FC */ ActorEventRelated mActorEvent;
+    /* 0x14C */ mMtx_c mMatrix;
+    /* 0x17C */ u32 mEventId;
+    /* 0x180 */ u32 mSetSceneflag;
+    /* 0x184 */ u32 mTrigSceneflag;
+    /* 0x188 */ u32 mSubtype;
+    /* 0x18C */ u32 mUnkParamFlag;
 };
 #endif
