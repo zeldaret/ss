546b19db31dcc7fbac20a186468733eed37bad9e
MaxMotis <mr.maxmotis@gmail.com>
2026-03-09 21:55:09 +0100

    d_t_event_f OK
    not yet looked into proper member variable names

---
+++ include/d/t/d_t_event_f.h
@@ -0,0 +1,33 @@
+#ifndef D_T_EVENT_F_H
+#define D_T_EVENT_F_H
+
+#include "common.h"
+#include "d/flag/storyflag_manager.h"
+#include "d/t/d_tg.h"
+#include "m/m_mtx.h"
+#include "toBeSorted/actor_event.h"
+
+class dTgEventF_c : public dTg_c {
+public:
+    dTgEventF_c() : mActorEvent(*this, nullptr) {}
+    virtual ~dTgEventF_c() {}
+
+    virtual int create() override;
+    virtual int doDelete() override;
+    static int checkStoryflagValid(u32);
+    static bool checkUncommitedStoryFlagValue(u32);
+    static void setStoryFlag(StoryFlags_e);
+    static void unsetStoryFlag(StoryFlags_e);
+    virtual int actorExecute() override;
+    virtual int actorExecuteInEvent() override;
+    virtual int draw() override;
+
+private:
+    /* 0x0FC */ ActorEventRelated mActorEvent;
+    /* 0x14C */ mMtx_c mMatrix;
+    /* 0x17C */ u32 mEventId;
+    /* 0x180 */ u32 p1_s8_0x7FF;
+    /* 0x184 */ u32 p1_s19_0x7FF;
+    /* 0x188 */ u32 p1_s30_0x3;
+};
+#endif
