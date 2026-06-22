895ee9eb032ceae947b717b9008739bdb8e24865
compugab <compugab@hotmail.com>
2026-06-22 13:42:28 -0400

    matching d_lyt_map_capture and helping functions in d_stage

--- src/d/lyt/d_lyt_map_capture.cpp
+++ src/d/lyt/d_lyt_map_capture.cpp
@@ -1,4 +1,5 @@
 #include "d/lyt/d_lyt_map_capture.h"
+
 #include "common.h"
 #include "d/d_stage.h"
 #include "d/lyt/d_lyt_map_global.h"
@@ -19,7 +20,8 @@ void dLytMapCapture_c::finalizeState_RenderingWait() {}
 
 void dLytMapCapture_c::initializeState_RenderingWaitStep2() {}
 void dLytMapCapture_c::executeState_RenderingWaitStep2() {
-    if (mIsBusyRendering && dStage_c::GetInstance() != nullptr && !dStage_c::GetInstance()->getMapRelated()->getField_0x1EE()) {
+    if (mIsBusyRendering && dStage_c::GetInstance() != nullptr &&
+        !dStage_c::GetInstance()->getMapRelated()->getField_0x1EE()) {
         mIsBusyRendering = false;
         if (mRenderRequest) {
             fn_8012D6F0();
@@ -32,45 +34,25 @@ void dLytMapCapture_c::executeState_RenderingWaitStep2() {
 }
 void dLytMapCapture_c::finalizeState_RenderingWaitStep2() {}
 
-bool dLytMapCapture_c::isBusyRendering() const{
-    return mIsBusyRendering;
+void dLytMapCapture_c::fn_8012D610(mVec3_c *out) {
+    dStage_c::GetInstance()->getMapRelated()->fn_801B50C0(0);
+    *out = dStage_c::GetInstance()->getMapRelated()->fn_801B4CB0();
 }
 
-void dLytMapCapture_c::execute() {
-    mStateMgr.executeState();
+void dLytMapCapture_c::fn_8012D670(mVec3_c *out) {
+    dStage_c::GetInstance()->getMapRelated()->fn_801B50C0(0);
+    *out = dStage_c::GetInstance()->getMapRelated()->fn_801B4C90();
 }
 
 void dLytMapCapture_c::fn_8012D6D0(mVec3_c *out) {
     *out = dStage_c::GetInstance()->getMapRelated()->field_0x0FC;
 }
 
-void dLytMapCapture_c::fn_8012D610(mVec3_c *out){
-    dStage_c::GetInstance()->getMapRelated()->fn_801B50C0(0);
-    *out = dStage_c::GetInstance()->getMapRelated()->fn_801B4CB0();
-}
+void dLytMapCapture_c::fn_8012D6F0() {
+    dStage_c *pStage = dStage_c::GetInstance();
 
-void dLytMapCapture_c::fn_8012D670(mVec3_c *out){
-    dStage_c::GetInstance()->getMapRelated()->fn_801B50C0(0);
-    *out = dStage_c::GetInstance()->getMapRelated()->fn_801B4C90();
-}
-
-
-void dLytMapCapture_c::fn_8012D800(nw4r::lyt::Pane *param_2, const GXTexObj *param_3) {
-    if (param_3 != nullptr && dStage_c::GetInstance() != nullptr && dStage_c::GetInstance()->getMapRelated()->fn_801B5970(param_3)) {
-        field_0x070.width = GXGetTexObjWidth(param_3);
-        field_0x070.height = GXGetTexObjHeight(param_3);
-        param_2->SetSize(field_0x070);
-        param_2->GetMaterial()->GetTexMapAry()->ReplaceImage(*param_3);
-    }
-}
-
-
-void dLytMapCapture_c::fn_8012D6F0(){
- 
-    dStage_c * pStage = dStage_c::GetInstance();
-    
     if (pStage != nullptr) {
-        dLytMapGlobal_c* pLytMap = dLytMapGlobal_c::GetInstance();
+        dLytMapGlobal_c *pLytMap = dLytMapGlobal_c::GetInstance();
 
         f32 zoom = pLytMap->getZoomFrame();
         f32 inv = 1.0f / pLytMap->getField_0x44();
@@ -79,16 +61,33 @@ void dLytMapCapture_c::fn_8012D6F0(){
         mVec3_c v(field_0x064 * inv, 1, field_0x06C * inv);
 
         pStage->getMapRelated()->fn_801B50C0(floor);
- 
+
         pStage->getMapRelated()->field_0x108 = pLytMap->getMapScroll();
         pStage->getMapRelated()->field_0x114 = v;
-         
-        pStage->getMapRelated()->field_0x120 =  pLytMap->getMapRotationCenter(); 
-        
-        
+
+        pStage->getMapRelated()->field_0x120 = pLytMap->getMapRotationCenter();
+
         pStage->getMapRelated()->field_0x1E6 = (u16)(-pLytMap->getMapRotation());
         pStage->getMapRelated()->field_0x12C = zoom;
         pStage->getMapRelated()->field_0x1EE = 1;
     }
     return;
-} 
+}
+
+void dLytMapCapture_c::execute() {
+    mStateMgr.executeState();
+}
+
+bool dLytMapCapture_c::isBusyRendering() const {
+    return mIsBusyRendering;
+}
+
+void dLytMapCapture_c::fn_8012D800(nw4r::lyt::Pane *param_2, GXTexObj *param_3) {
+    if (param_3 != nullptr && dStage_c::GetInstance() != nullptr &&
+        dStage_c::GetInstance()->getMapRelated()->fn_801B5970(param_3)) {
+        field_0x070.width = GXGetTexObjWidth(param_3);
+        field_0x070.height = GXGetTexObjHeight(param_3);
+        param_2->SetSize(field_0x070);
+        param_2->GetMaterial()->GetTexMapAry()->ReplaceImage(*param_3);
+    }
+}
